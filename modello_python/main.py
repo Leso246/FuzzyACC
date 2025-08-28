d9abb8495da087f0731c575ba8fd444352b3c7d1
Leso246 <simolesi02@gmail.com>
2025-08-28 17:18:47 +0200

    Fix: tolto main commentato

--- modello_python/main.py
+++ modello_python/main.py
@@ -8,42 +8,42 @@ from generate_sim_plots import generate_sim_plots
 if __name__ == "__main__":
     system, weather_condition, time_headway, relative_velocity, acceleration = create_fuzzy_system()
 
-    # # Plotta le Membership Functrions
-    # generate_mf_plots(
-    #     weather_condition, 
-    #     "Weather Condition", 
-    #     "Weather Condition (0=Bad, 1=Good)", 
-    #     "/weather_condition", 
-    #     tight_legend=False
-    # )
+    # Plotta le Membership Functrions
+    generate_mf_plots(
+        weather_condition, 
+        "Weather Condition", 
+        "Weather Condition (0=Bad, 1=Good)", 
+        "/weather_condition", 
+        tight_legend=False
+    )
     
-    # generate_mf_plots(
-    #     time_headway, 
-    #     "Time Headway", 
-    #     "Time Headway [s]", 
-    #      "/time_headway", 
-    #     tight_legend=False
-    # )
+    generate_mf_plots(
+        time_headway, 
+        "Time Headway", 
+        "Time Headway [s]", 
+         "/time_headway", 
+        tight_legend=False
+    )
 
-    # generate_mf_plots(
-    #     relative_velocity, 
-    #     "Relative Velocity", 
-    #     "Relative Velocity [m/s]", 
-    #     "/relative_velocity", 
-    #     tight_legend=False
-    # )
+    generate_mf_plots(
+        relative_velocity, 
+        "Relative Velocity", 
+        "Relative Velocity [m/s]", 
+        "/relative_velocity", 
+        tight_legend=False
+    )
 
-    # generate_mf_plots(
-    #     acceleration, 
-    #     "Acceleration", 
-    #     "Accelerazione [m/s²]", 
-    #     "/acceleration", 
-    #     tight_legend=True
-    # )
+    generate_mf_plots(
+        acceleration, 
+        "Acceleration", 
+        "Accelerazione [m/s²]", 
+        "/acceleration", 
+        tight_legend=True
+    )
 
 
-    # # Avvia la simulazione
-    # run_simulation(system)
+    # Avvia la simulazione
+    run_simulation(system)
 
     # Genera i plots relativi ai dati prodotti dalla simulazione
     generate_sim_plots()
